44349add21267e5c158a8b1cd9b852894181c826
Akhilesh <akhilesh.soni@somaiya.edu>
2023-10-10 20:37:25 +0530

    changes done

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),  # Added a name for the index page
     path('simulation/', views.simulation, name='simulation'),
 ]
